b3e337b5fe8b19ee71af06c0a6a469f5237e5284
Omar Ashraf <Omarlabib.51237@gmail.com>
2024-03-08 15:21:23 +0200

    adding hikari

---
+++ src/main/java/iti/jets/jetshop/Test.java
@@ -0,0 +1,21 @@
+package iti.jets.jetshop;
+
+import iti.jets.jetshop.Persistence.Entities.Category;
+import iti.jets.jetshop.Persistence.EntityFactory;
+import jakarta.persistence.EntityManager;
+
+
+public class Test {
+    public static void main(String[] args) {
+        EntityManager obj = EntityFactory.getInstance().createEntityManager();
+
+
+        Category cat = new Category();
+        cat.setCategoryName("test");
+        obj.getTransaction().begin();
+        obj.persist(cat);
+        obj.getTransaction().commit();
+        obj.close();
+
+    }
+}
